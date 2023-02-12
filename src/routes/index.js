f572e6baad4cb2b0c2aabf084374d8d5449b1084
anonymous-sys19 <estudiosrg5@gmail.com>
2023-02-12 09:00:11 -0600

    new pull pull new

---
+++ src/routes/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router()
+const upload = require("../controllers/index")
+const rvr = require('../public/RVR/rvres.json')
+const book = 'Enoc_libro/LibrodeEnoc.pdf';
+const path = require('path')
+
+
+// Publiccaciones
+
+router.get('/imagenes/:id', (req, res) => {
+    res.sendFile(`imagenes/${id}`)
+    
+});
+
+//Routing
+router.get('/', (req, res) => {
+    res.render('home', { book })
+});
+
+//Uploading GET
+router.get('/new-upload-file', (req, res) => {
+    res.render('upload/uploaded')
+});
+
+// UPLOADING POST
+router.post('/upload-file', upload.array('uploaded_file'), function (req, res) {
+
+    console.log(req.file)
+    res.redirect('/')
+});
+
+
+//Files book enock
+router.get('/book', (req, res) => {
+
+    res.render('book/index', { book })
+
+});
+//SANTA BIBLIA
+router.get('/bib', (req, res) => {
+
+});
+
+module.exports = router;
+// recorrer json con nodejs?
+
+//
