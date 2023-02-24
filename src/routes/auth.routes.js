c68864c7a6df1683d7b8b27fd67a1822b0e5647b
anonymous-sys19 <estudiosrg5@gmail.com>
2023-02-24 15:35:57 -0600

    Main new login pull

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,8 @@
 const { verifySignUp } = require("../middlewares");
 const controller = require("../controllers/auth.controller");
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
+module.exports = function (app) {
+  app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, Content-Type, Accept"
@@ -10,14 +10,9 @@ module.exports = function(app) {
     next();
   });
 
-  app.post(
-    "/api/auth/signup",
-    [
-      verifySignUp.checkDuplicateUsernameOrEmail,
-      verifySignUp.checkRolesExisted
-    ],
-    controller.signup
-  );
+  app.post("/api/auth/signup", [verifySignUp.checkDuplicateUsernameOrEmail, verifySignUp.checkRolesExisted], controller.signup, (req, res) => {
+    res.redirect('/')
+  });
 
   app.post("/api/auth/signin", controller.signin);
 
